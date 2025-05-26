8b87c073acbb449a58499932b1d9019354d427f8
minato369 <87985403+minato369@users.noreply.github.com>
2025-05-26 20:40:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: import.meta.env.VITE_BASE_PAT || '/Multiform',
+	base: import.meta.env.VITE_BASE_PATH || '/Multiform',
 	plugins: [
 		react(),
 		tailwindcss()
