cab133b9b178f7e1851e8e1df47c1cbe8b39ed5c
Abhishek <abhishekkamble369@gmail.com>
2025-05-26 12:33:04 +0530

    fixes

--- src/component/SideBar/Sidebar.jsx
+++ src/component/SideBar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Sidebar.css';
+import './SideBar.css';
 
 const Sidebar = ({activeStep}) => {
 	// const [activeStep, setActiveStep] = useState(1);
